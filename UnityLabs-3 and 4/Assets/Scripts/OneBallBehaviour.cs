b501dae3b6b866946db53db09632fe7a5bf20e75
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-09 10:00:05 +0200

    create new unity lab, making a new game

---
+++ UnityLabs-3 and 4/Assets/Scripts/OneBallBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneBallBehaviour : MonoBehaviour
+{
+    public float XRotation = 0;
+    public float YRotation = 1;
+    public float ZRotation = 0;
+    public float DegreesPerSecond = 180;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        transform.RotateAround(Vector3.zero, axis, DegreesPerSecond);
+        // Debug.DrawRay(Vector3.zero, axis, Color.yellow);
+    }
+}
