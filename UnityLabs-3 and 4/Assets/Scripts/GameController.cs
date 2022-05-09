61cb201236cb75331faf4982fe90051b0ae66ca8
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-09 10:45:29 +0200

    the game now adds new balls every 1.5 seconds, and the balls have physics

--- UnityLabs-3 and 4/Assets/Scripts/GameController.cs
+++ UnityLabs-3 and 4/Assets/Scripts/GameController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject OneBallPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("AddABall", 1.5f, 1);
+    }
+
+    void AddABall()
+    {
+        Instantiate(OneBallPrefab);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
