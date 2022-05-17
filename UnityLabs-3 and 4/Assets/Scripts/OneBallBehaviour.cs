7e374dd08344ae026d033d356dfe60ebbe56db66
Kieran Taylor <kmtaylor00@outlook.com>
2022-05-17 13:45:53 +0200

    The balls now jump around in random positions

--- UnityLabs-3 and 4/Assets/Scripts/OneBallBehaviour.cs
+++ UnityLabs-3 and 4/Assets/Scripts/OneBallBehaviour.cs
@@ -10,14 +10,38 @@ public class OneBallBehaviour : MonoBehaviour
     public float Multiplier = 0.75F;
     private static int BallCount = 0;
     public int BallNumber;
+    public float TooFar = 5F;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
         BallCount++;
         BallNumber = BallCount;
         ResetBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * XSpeed, Time.deltaTime * YSpeed, Time.deltaTime * ZSpeed);
+
+        XSpeed += Multiplier - Random.value * Multiplier * 2;
+        YSpeed += Multiplier - Random.value * Multiplier * 2;
+        ZSpeed += Multiplier - Random.value * Multiplier * 2;
+
+        if ((Mathf.Abs(transform.position.x) > TooFar) || (Mathf.Abs(transform.position.y) > TooFar) || (Mathf.Abs(transform.position.z) > TooFar))
+        {
+            ResetBall();
+        }
+    }
+
+    private void ResetBall()
+    {
+        XSpeed = Multiplier - Random.value * Multiplier;
+        YSpeed = Multiplier - Random.value * Multiplier;
+        ZSpeed = Multiplier - Random.value * Multiplier;
+
+        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
     }
 
     private void OnMouseDown()
@@ -30,18 +54,4 @@ public class OneBallBehaviour : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Time.deltaTime * XSpeed, Time.deltaTime * YSpeed, Time.deltaTime * ZSpeed);
-
-        XSpeed += Multiplier - Random.value * Multiplier * 2;
-        YSpeed += Multiplier - Random.value * Multiplier * 2;
-        ZSpeed += Multiplier - Random.value * Multiplier * 2;
-    }
-
-    private void ResetBall()
-    {
-        XSpeed = Multiplier - Random.value * Multiplier * 2;
-    }
 }
